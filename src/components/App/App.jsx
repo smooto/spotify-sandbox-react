3803d00a1e5e28f2b53ef1dbe48ac5107c691b34
smooto <melissasmoot@live.com>
2020-08-18 14:50:06 -0700

    Set up basic form for socket demo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  const [newUri, setNewUri] = useState('');
+  const [uriArray, setUriArray] = useState(['']);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setUriArray(uriArray.concat(newUri));
+  };
+
+  const handleChange = ({ target }) => {
+    setNewUri(target.value);
+  };
+
+  return (
+    <>
+      <form onSubmit={e => handleSubmit(e)}>
+        <input type="text" onChange={e => handleChange(e)}></input>
+        <input type="submit"></input>
+      </form>
+      <ul>
+        {
+          uriArray.map((uri, i) => <li key={i}>{uri}</li>)
+        }
+      </ul>
+    </>
+  );
 }
-  
